d779f8dd2ab94288fdb83d502a6077fc50322741
Ferruh Yigit <ferruh.yigit@intel.com>
2016-06-01 18:39:21 +0100

    spp_vm: remove unused struct client definition
    Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>
    Acked-by: Sy Jong Choi <sy.jong.choi@intel.com>

--- examples/multi_process/patch_panel/vm/init.h
+++ examples/multi_process/patch_panel/vm/init.h
@@ -36,25 +36,6 @@
 
 #include <stdint.h>
 
-/*
- * Define a client structure with all needed info, including
- * stats from the clients.
- */
-struct client {
-	struct rte_ring *rx_q;
-	unsigned int client_id;
-	/*
-	 * these stats hold how many packets the client will actually receive,
-	 * and how many packets were dropped because the client's queue was full
-	 * The port-info stats, in contrast, record how many packets were
-	 * received or transmitted on an actual NIC port.
-	 */
-	struct {
-		uint64_t rx;
-		uint64_t rx_drop;
-	} stats;
-};
-
 /* the shared port information: port numbers, rx and tx stats etc. */
 extern struct port_info *ports;
 
