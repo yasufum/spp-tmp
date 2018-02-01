yasufum/spp-tmp

commit 11512d8b6c28bc99648c89a6009f13b15f963ba2

spp_vf: add vf functions
* Classifier by MAC address
* Forwarder
* Merger

Signed-off-by: Tomoyuki Mizuguchi <mizuguchi.tomoyuki@po.ntt-tx.co.jp>
Signed-off-by: Yasufum Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

src/vf/classifier_mac.c
@@ -1,0 +1,233 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+
+#include <rte_mbuf.h>
+#include <rte_log.h>
+#include <rte_cycles.h>
+#include <rte_malloc.h>
+#include <rte_memcpy.h>
+#include <rte_random.h>
+#include <rte_byteorder.h>
+#include <rte_per_lcore.h>
+#include <rte_eal.h>
+#include <rte_launch.h>
+#include <rte_hash.h>
+
+#include "spp_vf.h"
+#include "ringlatencystats.h"
+#include "classifier_mac.h"
+
+#define RTE_LOGTYPE_SPP_CLASSIFIER_MAC RTE_LOGTYPE_USER1
+
+#ifdef RTE_MACHINE_CPUFLAG_SSE4_2
+#include <rte_hash_crc.h>
+#define DEFAULT_HASH_FUNC rte_hash_crc
+#else
+#include <rte_jhash.h>
+#define DEFAULT_HASH_FUNC rte_jhash
+#endif
+
+/* number of classifier mac table entry */
+#define NUM_CLASSIFIER_MAC_TABLE_ENTRY 128
+
+/* interval that transmit burst packet, if buffer is not filled.
+		nano second */
+#define DRAIN_TX_PACKET_INTERVAL 100
+
+/* mac address string(xx:xx:xx:xx:xx:xx) buffer size */
+static const size_t ETHER_ADDR_STR_BUF_SZ =
+		ETHER_ADDR_LEN * 2 + (ETHER_ADDR_LEN - 1) + 1;
+
+/* classified data (destination port, target packets, etc) */
+struct classified_data {
+	int      if_no;
+	uint8_t  tx_port;
+	uint16_t num_pkt;
+	struct rte_mbuf *pkts[MAX_PKT_BURST];
+};
+
+/* initialize classifier. */
+static int
+init_classifier(const struct spp_core_info *core_info,
+		struct rte_hash **classifier_mac_table, struct classified_data *classified_data)
+{
+	int ret = -1;
+	int i;
+	struct ether_addr eth_addr;
+	char mac_addr_str[ETHER_ADDR_STR_BUF_SZ];
+
+	/* set hash creating parameters */
+	struct rte_hash_parameters hash_params = {
+			.name      = "classifier_mac_table",
+			.entries   = NUM_CLASSIFIER_MAC_TABLE_ENTRY,
+			.key_len   = sizeof(struct ether_addr),
+			.hash_func = DEFAULT_HASH_FUNC,
+			.hash_func_init_val = 0,
+			.socket_id = rte_socket_id(),
+	};
+
+#ifdef RTE_MACHINE_CPUFLAG_SSE4_2
+	RTE_LOG(DEBUG, SPP_CLASSIFIER_MAC, "Enabled SSE4.2. use crc hash.\n");
+#else
+	RTE_LOG(DEBUG, SPP_CLASSIFIER_MAC, "Disabled SSE4.2. use jenkins hash.\n");
+#endif
+
+	/* create classifier mac table (hash table) */
+	*classifier_mac_table = rte_hash_create(&hash_params);
+	if (unlikely(*classifier_mac_table == NULL)) {
+		RTE_LOG(ERR, SPP_CLASSIFIER_MAC, "Cannot create classifier mac table\n");
+		return -1;
+	}
+
+	/* populate the hash */
+	for (i = 0; i < core_info->num_tx_port; i++) {
+		rte_memcpy(&eth_addr, &core_info->tx_ports[i].mac_addr, ETHER_ADDR_LEN);
+
+		/* add entry to classifier mac table */
+		ret = rte_hash_add_key_data(*classifier_mac_table, (void*)&eth_addr, (void*)(long)i);
+		if (unlikely(ret < 0)) {
+			ether_format_addr(mac_addr_str, sizeof(mac_addr_str), &eth_addr);
+			RTE_LOG(ERR, SPP_CLASSIFIER_MAC,
+					"Cannot add entry to classifier mac table. "
+					"ret=%d, mac_addr=%s\n", ret, mac_addr_str);
+			rte_hash_free(*classifier_mac_table);
+			*classifier_mac_table = NULL;
+			return -1;
+		}
+
+		/* set value */
+		classified_data[i].if_no   = i;
+		classified_data[i].tx_port = core_info->tx_ports[i].dpdk_port;
+		classified_data[i].num_pkt = 0;
+	}
+
+	return 0;
+}
+
+/* transimit packet to one destination. */
+static inline void
+transimit_packet(struct classified_data *classified_data)
+{
+	int i;
+	uint16_t n_tx;
+
+	/* set ringlatencystats */
+	spp_ringlatencystats_add_time_stamp(classified_data->if_no,
+			classified_data->pkts, classified_data->num_pkt);
+
+	/* transimit packets */
+	n_tx = rte_eth_tx_burst(classified_data->tx_port, 0,
+			classified_data->pkts, classified_data->num_pkt);
+
+	/* free cannnot transiit packets */
+	if (unlikely(n_tx != classified_data->num_pkt)) {
+		for (i = n_tx; i < classified_data->num_pkt; i++)
+			rte_pktmbuf_free(classified_data->pkts[i]);
+		RTE_LOG(DEBUG, SPP_CLASSIFIER_MAC,
+				"drop packets(tx). num=%hu, dpdk_port=%hhu\n",
+				classified_data->num_pkt - n_tx, classified_data->tx_port);
+	}
+
+	classified_data->num_pkt = 0;
+}
+
+/* classify packet by destination mac address,
+		and transimit packet (conditional). */
+static inline void
+classify_packet(struct rte_mbuf **rx_pkts, uint16_t n_rx,
+		struct rte_hash *classifier_mac_table, struct classified_data *classified_data)
+{
+	int ret;
+	int i;
+	struct ether_hdr *eth;
+	struct classified_data *cd;
+	void *lookup_data;
+	char mac_addr_str[ETHER_ADDR_STR_BUF_SZ];
+
+	for (i = 0; i < n_rx; i++) {
+		eth = rte_pktmbuf_mtod(rx_pkts[i], struct ether_hdr *);
+
+		/* find in table (by destination mac address)*/
+		ret = rte_hash_lookup_data(classifier_mac_table,
+				(const void*)&eth->d_addr, &lookup_data);
+		if (unlikely(ret < 0)) {
+			ether_format_addr(mac_addr_str, sizeof(mac_addr_str), &eth->d_addr);
+			RTE_LOG(ERR, SPP_CLASSIFIER_MAC,
+					"unknown mac address. ret=%d, mac_addr=%s\n", ret, mac_addr_str);
+			rte_pktmbuf_free(rx_pkts[i]);
+			continue;
+		}
+
+		/* set mbuf pointer to tx buffer */
+		cd = classified_data + (long)lookup_data;
+		cd->pkts[cd->num_pkt++] = rx_pkts[i];
+
+		/* transimit packet, if buffer is filled */
+		if (unlikely(cd->num_pkt == MAX_PKT_BURST))
+			transimit_packet(cd);
+	}
+}
+
+/* classifier(mac address) thread function. */
+int
+spp_classifier_mac_do(void *arg)
+{
+	int ret = -1;
+	int i;
+	int n_rx;
+	struct spp_core_info *core_info = (struct spp_core_info *)arg;
+	struct rte_mbuf *rx_pkts[MAX_PKT_BURST];
+
+	struct rte_hash *classifier_mac_table = NULL;
+	const int n_classified_data = core_info->num_tx_port;
+	struct classified_data classified_data[n_classified_data];
+
+	uint64_t cur_tsc, prev_tsc = 0;
+	const uint64_t drain_tsc = (rte_get_tsc_hz() + US_PER_S - 1) /
+			US_PER_S * DRAIN_TX_PACKET_INTERVAL;
+
+	/* initialize */
+	ret = init_classifier(core_info, &classifier_mac_table, classified_data);
+	if (unlikely(ret != 0))
+		return ret;
+
+	/* to idle  */
+	core_info->status = SPP_CORE_IDLE;
+	while(likely(core_info->status == SPP_CORE_IDLE) ||
+			likely(core_info->status == SPP_CORE_FORWARD)) {
+
+		while(likely(core_info->status == SPP_CORE_FORWARD)) {
+			/* drain tx packets, if buffer is not filled for interval */
+			cur_tsc = rte_rdtsc();
+			if (unlikely(cur_tsc - prev_tsc > drain_tsc)) {
+				for (i = 0; i < n_classified_data; i++) {
+					if (unlikely(classified_data[i].num_pkt != 0))
+						transimit_packet(&classified_data[i]);
+				}
+				prev_tsc = cur_tsc;
+			}
+
+			/* retrieve packets */
+			n_rx = rte_eth_rx_burst(core_info->rx_ports[0].dpdk_port, 0,
+					rx_pkts, MAX_PKT_BURST);
+			if (unlikely(n_rx == 0))
+				continue;
+
+			/* classify and transimit (filled) */
+			classify_packet(rx_pkts, n_rx, classifier_mac_table, classified_data);
+		}
+	}
+
+	/* uninitialize */
+	if (classifier_mac_table != NULL) {
+		rte_hash_free(classifier_mac_table);
+		classifier_mac_table = NULL;
+	}
+	core_info->status = SPP_CORE_STOP;
+
+	return 0;
+}

src/vf/Makefile
@@ -34,13 +34,23 @@ $(error "Please define RTE_SDK environment variable")
 endif
 
 # Default target, can be overriden by command line or environment
-RTE_TARGET ?= x86_64-native-linuxapp-gcc
-
 include $(RTE_SDK)/mk/rte.vars.mk
 
-DIRS-$(CONFIG_RTE_EXEC_ENV_LINUXAPP) += nfv
-DIRS-$(CONFIG_RTE_EXEC_ENV_LINUXAPP) += primary
-DIRS-$(CONFIG_RTE_EXEC_ENV_LINUXAPP) += vm
-DIRS-$(CONFIG_RTE_EXEC_ENV_LINUXAPP) += vf
+# binary name
+APP = spp_vf
 
-include $(RTE_SDK)/mk/rte.extsubdir.mk
+# all source are stored in SRCS-y
+SRCS-y := spp_vf.c spp_config.c classifier_mac.c spp_forward.c ringlatencystats.c ../shared/common.c
+
+CFLAGS += $(WERROR_FLAGS) -O3
+CFLAGS += -I$(SRCDIR)/../shared
+#CFLAGS += -DSPP_DEMONIZE
+#CFLAGS += -DSPP_RINGLATENCYSTATS_ENABLE
+
+LDLIBS += -ljansson
+ifeq ($(CONFIG_RTE_BUILD_SHARED_LIB),y)
+LDLIBS += -lrte_pmd_ring
+LDLIBS += -lrte_pmd_vhost
+endif
+
+include $(RTE_SDK)/mk/rte.extapp.mk

src/vf/classifier_mac.h
@@ -1,0 +1,12 @@
+#ifndef _CLASSIFIER_MAC_H_
+#define _CLASSIFIER_MAC_H_
+
+/**
+ * classifier(mac address) thread function.
+ *
+ * @param arg
+ *  pointer to struct spp_core_info.
+ */
+int spp_classifier_mac_do(void *arg);
+
+#endif /* _CLASSIFIER_MAC_H_ */

src/vf/ringlatencystats.c
@@ -1,0 +1,147 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <math.h>
+
+#include <rte_mbuf.h>
+#include <rte_log.h>
+#include <rte_cycles.h>
+#include <rte_malloc.h>
+#include <rte_memcpy.h>
+
+#include "ringlatencystats.h"
+
+#define NS_PER_SEC 1E9
+
+#define RTE_LOGTYPE_SPP_RING_LATENCY_STATS RTE_LOGTYPE_USER1
+
+#ifdef SPP_RINGLATENCYSTATS_ENABLE
+
+/** ring latency statistics information */
+struct ring_latency_stats_info {
+	uint64_t timer_tsc;   /**< sampling interval counter */
+	uint64_t prev_tsc;    /**< previous time */
+	struct spp_ringlatencystats_ring_latency_stats stats;  /**< ring latency statistics list */
+};
+
+/** sampling interval */
+static uint64_t g_samp_intvl = 0;
+
+/** ring latency statistics information instance */
+static struct ring_latency_stats_info *g_stats_info = NULL;
+
+/** number of ring latency statisics */
+static uint16_t g_stats_count = 0;
+
+/* clock cycles per nano second */
+static inline uint64_t
+cycles_per_ns(void)
+{
+	return rte_get_timer_hz() / NS_PER_SEC;
+}
+
+int
+spp_ringlatencystats_init(uint64_t samp_intvl, uint16_t stats_count)
+{
+	/* allocate memory for ring latency statisics infromation */
+	g_stats_info = rte_zmalloc(
+			"global ring_latency_stats_info",
+			sizeof(struct ring_latency_stats_info) * stats_count, 0);
+	if (unlikely(g_stats_info == NULL)) {
+		RTE_LOG(ERR, SPP_RING_LATENCY_STATS,
+				"Cannot allocate memory for ring latency stats info\n");
+		return -1;
+	}
+
+	/* store global information for ring latency statistics */
+	g_samp_intvl = samp_intvl * cycles_per_ns();
+	g_stats_count = stats_count;
+
+	RTE_LOG(DEBUG, SPP_RING_LATENCY_STATS,
+			"g_samp_intvl=%lu, g_stats_count=%hu, cpns=%lu\n",
+			g_samp_intvl, g_stats_count, cycles_per_ns());
+
+	return 0;
+}
+
+void
+spp_ringlatencystats_uninit(void)
+{
+	/* free memory for ring latency statisics infromation */
+	if (likely(g_stats_info != NULL)) {
+		rte_free(g_stats_info);
+		g_stats_count = 0;
+	}
+}
+
+void
+spp_ringlatencystats_add_time_stamp(int ring_id,
+			struct rte_mbuf **pkts, uint16_t nb_pkts)
+{
+	unsigned int i;
+	uint64_t diff_tsc, now;
+	struct ring_latency_stats_info *stats_info = &g_stats_info[ring_id];
+
+	for (i = 0; i < nb_pkts; i++) {
+
+		/* get tsc now */
+		now = rte_rdtsc();
+
+		/* calculate difference from the previous processing time */
+		diff_tsc = now - stats_info->prev_tsc;
+		stats_info->timer_tsc += diff_tsc;
+
+		/* when it is over sampling interval */
+		/* set tsc to mbuf::timestamp */
+		if (unlikely(stats_info->timer_tsc >= g_samp_intvl)) {
+			pkts[i]->timestamp = now;
+			stats_info->timer_tsc = 0;
+		}
+
+		/* update previus tsc */
+		stats_info->prev_tsc = now;
+	}
+}
+
+void
+spp_ringlatencystats_calculate_latency(int ring_id,
+			struct rte_mbuf **pkts, uint16_t nb_pkts)
+{
+	unsigned int i;
+	uint64_t now;
+	int64_t latency;
+	struct ring_latency_stats_info *stats_info = &g_stats_info[ring_id];
+
+	now = rte_rdtsc();
+	for (i = 0; i < nb_pkts; i++) {
+		if (likely(pkts[i]->timestamp == 0))
+			continue;
+
+		/* when mbuf::timestamp is not zero */
+		/* calculate latency */
+		latency = (uint64_t)floor((now - pkts[i]->timestamp) / cycles_per_ns());
+		if (likely(latency < SPP_RINGLATENCYSTATS_STATS_SLOT_COUNT-1))
+			stats_info->stats.slot[latency]++;
+		else
+			stats_info->stats.slot[SPP_RINGLATENCYSTATS_STATS_SLOT_COUNT-1]++;
+	}
+}
+
+int
+spp_ringlatencystats_get_count(void)
+{
+	return g_stats_count;
+}
+
+void
+spp_ringlatencystats_get_stats(int ring_id,
+		struct spp_ringlatencystats_ring_latency_stats *stats)
+{
+	struct ring_latency_stats_info *stats_info = &g_stats_info[ring_id];
+
+	rte_memcpy(stats, &stats_info->stats,
+			sizeof(struct spp_ringlatencystats_ring_latency_stats));
+}
+
+#endif /* SPP_RINGLATENCYSTATS_ENABLE */

src/vf/ringlatencystats.h
@@ -1,0 +1,69 @@
+#ifndef _RINGLATENCYSTATS_H_
+#define _RINGLATENCYSTATS_H_
+
+#include <rte_mbuf.h>
+
+/** number of slots to save latency. 0ns~99ns and 100ns over */
+#define SPP_RINGLATENCYSTATS_STATS_SLOT_COUNT 101
+
+/** ring latency statistics */
+struct spp_ringlatencystats_ring_latency_stats {
+	uint64_t slot[SPP_RINGLATENCYSTATS_STATS_SLOT_COUNT]; /**< slots to save latency */
+};
+
+
+#ifdef SPP_RINGLATENCYSTATS_ENABLE
+/**
+ * initialize ring latency statisics.
+ *
+ * @retval 0: succeeded.
+ * @retval -1: failed.
+ */
+int spp_ringlatencystats_init(uint64_t samp_intvl, uint16_t stats_count);
+
+/**
+ *uninitialize ring latency statisics.
+ */
+void spp_ringlatencystats_uninit(void);
+
+/**
+ * add time-stamp to mbuf's member.
+ *
+ * call at enqueue.
+ */
+void spp_ringlatencystats_add_time_stamp(int ring_id,
+			struct rte_mbuf **pkts, uint16_t nb_pkts);
+
+/**
+ * calculate latency.
+ *
+ * call at dequeue.
+ */
+void spp_ringlatencystats_calculate_latency(int ring_id,
+			struct rte_mbuf **pkts, uint16_t nb_pkts);
+
+/**
+ * get number of ring latency statisics.
+ *
+ * @return spp_ringlatencystats_init's parameter "stats_count"
+ */
+int spp_ringlatencystats_get_count(void);
+
+/**
+ *get specific ring latency statisics.
+ */
+void spp_ringlatencystats_get_stats(int ring_id,
+		struct spp_ringlatencystats_ring_latency_stats *stats);
+
+#else
+
+#define spp_ringlatencystats_init(arg1, arg2) 0
+#define spp_ringlatencystats_uninit()
+#define spp_ringlatencystats_add_time_stamp(arg1, arg2, arg3)
+#define spp_ringlatencystats_calculate_latency(arg1, arg2, arg3)
+#define spp_ringlatencystats_get_count() 0
+#define spp_ringlatencystats_get_stats(arg1, arg2)
+
+#endif /* SPP_RINGLATENCYSTATS_ENABLE */
+
+#endif /* _RINGLATENCYSTATS_H_ */

commit 3498cc688cbc2fcf8e13dfbdde56c3298a9d2fec

spp_vf: fix build
build error:
...spp/src/vf/classifier_mac.c:314:5:
   error: format specifies type 'unsigned short' but the argument
          has type 'int' [-Werror,-Wformat]
	classified_data->num_pkt - n_tx, classified_data->port);
	^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
...build/include/rte_log.h:316:32:
   note: expanded from macro 'RTE_LOG'
	 RTE_LOGTYPE_ ## t, # t ": " __VA_ARGS__)
				     ^~~~~~~~~~~

Looks like caused by integer promotion, to fix cast variable
explicityly to uint16_t.

Fixes: 11512d8b6c28 ("spp_vf: add vf functions")

Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>
Acked-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

src/vf/classifier_mac.c
@@ -311,7 +311,8 @@ transmit_packet(struct classified_data *classified_data)
 			rte_pktmbuf_free(classified_data->pkts[i]);
 		RTE_LOG(DEBUG, SPP_CLASSIFIER_MAC,
 				"drop packets(tx). num=%hu, dpdk_port=%hu\n",
-				classified_data->num_pkt - n_tx, classified_data->port);
+				(uint16_t)(classified_data->num_pkt - n_tx),
+				classified_data->port);
 	}
 
 	classified_data->num_pkt = 0;