175926a20e1fe7929a9746b6e2b52a9a2d9351ae
Kenta Shinohara <shinohara.kenta@lab.ntt.co.jp>
2018-06-19 20:37:15 +0900

    spp_primary: fix deprecated use of rte_eth_dev_count
    This patch is same as previous one.
    
    Signed-off-by: Kenta Shinohara <shinohara.kenta@lab.ntt.co.jp>

--- src/primary/init.c
+++ src/primary/init.c
@@ -116,7 +116,7 @@ init(int argc, char *argv[])
 	argv += retval;
 
 	/* get total number of ports */
-	total_ports = rte_eth_dev_count();
+	total_ports = rte_eth_dev_count_avail();
 
 	/* set up array for port data */
 	if (rte_eal_process_type() == RTE_PROC_SECONDARY) {
