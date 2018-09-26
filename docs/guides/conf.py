a51b0ad6b8147dd9051a209aec9c572402caaf9d
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-09-26 18:08:35 +0900

    docs: add sphinx config to PDF
    To activate compile of PDF document, add attributes for latex
    to 'conf.py' of sphinx.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- docs/guides/conf.py
+++ docs/guides/conf.py
@@ -124,11 +124,12 @@ htmlhelp_basename = 'SoftPatchPaneldoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'letterpaper',
+    # 'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+    'pointsize': '11pt',
 
     # Additional stuff for the LaTeX preamble.
     #
@@ -136,7 +137,7 @@ latex_elements = {
 
     # Latex figure (float) alignment
     #
-    # 'figure_align': 'htbp',
+    'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
