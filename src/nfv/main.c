a0b460c44067eb14bc38908fccf7fe26519f3186
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-12-25 16:35:32 +0900

    spp_nfv: move params and funcs to header files
    Spp_nfv is one of the simplest secondaries in SPP, but still hard to
    understand the structure because all of codes are aggregated into one
    file. This update is to move variables and functions to header files,
    and these files are addes by next patches.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

---
+++ src/nfv/main.c
@@ -0,0 +1,247 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018  Nippon Telegraph and Telephone Corporation.
+ */
+
+#include <arpa/inet.h>
+#include <getopt.h>
+
+#include <rte_eth_ring.h>
+#include <rte_eth_vhost.h>
+#include <rte_memzone.h>
+
+#include "nfv.h"
+
+static sig_atomic_t on = 1;
+
+/*
+ * print a usage message
+ */
+static void
+usage(const char *progname)
+{
+	RTE_LOG(INFO, APP,
+		"Usage: %s [EAL args] -- -n <client_id>\n\n", progname);
+}
+
+/*
+ * Parse the application arguments to the client app.
+ */
+static int
+parse_app_args(int argc, char *argv[])
+{
+	int option_index, opt;
+	char **argvopt = argv;
+	const char *progname = argv[0];
+	static struct option lgopts[] = { {0} };
+	int ret;
+
+	while ((opt = getopt_long(argc, argvopt, "n:s:", lgopts,
+			&option_index)) != EOF) {
+		switch (opt) {
+		case 'n':
+			if (parse_num_clients(&client_id, optarg) != 0) {
+				usage(progname);
+				return -1;
+			}
+			break;
+		case 's':
+			ret = parse_server(&server_ip, &server_port, optarg);
+			if (ret != 0) {
+				usage(progname);
+				return -1;
+			}
+			break;
+		default:
+			usage(progname);
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+static void
+forward(void)
+{
+	uint16_t nb_rx;
+	uint16_t nb_tx;
+	int in_port;
+	int out_port;
+	uint16_t buf;
+	int i;
+
+	/* Go through every possible port numbers*/
+	for (i = 0; i < RTE_MAX_ETHPORTS; i++) {
+		struct rte_mbuf *bufs[MAX_PKT_BURST];
+
+		if (ports_fwd_array[i].in_port_id == PORT_RESET)
+			continue;
+
+		if (ports_fwd_array[i].out_port_id == PORT_RESET)
+			continue;
+
+		/* if status active, i count is in port*/
+		in_port = i;
+		out_port = ports_fwd_array[i].out_port_id;
+
+		/* Get burst of RX packets, from first port of pair. */
+		/*first port rx, second port tx*/
+		nb_rx = ports_fwd_array[in_port].rx_func(in_port, 0, bufs,
+			MAX_PKT_BURST);
+		if (unlikely(nb_rx == 0))
+			continue;
+
+		port_map[in_port].stats->rx += nb_rx;
+
+		/* Send burst of TX packets, to second port of pair. */
+		nb_tx = ports_fwd_array[out_port].tx_func(out_port, 0, bufs,
+			nb_rx);
+
+		port_map[out_port].stats->tx += nb_tx;
+
+		/* Free any unsent packets. */
+		if (unlikely(nb_tx < nb_rx)) {
+			port_map[out_port].stats->tx_drop += nb_rx - nb_tx;
+			for (buf = nb_tx; buf < nb_rx; buf++)
+				rte_pktmbuf_free(bufs[buf]);
+		}
+	}
+}
+
+/* main processing loop */
+static void
+nfv_loop(void)
+{
+	unsigned int lcore_id = rte_lcore_id();
+
+	RTE_LOG(INFO, APP, "entering main loop on lcore %u\n", lcore_id);
+
+	while (1) {
+		if (unlikely(cmd == STOP)) {
+			sleep(1);
+			/*RTE_LOG(INFO, APP, "Idling\n");*/
+			continue;
+		} else if (cmd == FORWARD) {
+			forward();
+		}
+	}
+}
+
+/* leading to nfv processing loop */
+static int
+main_loop(void *dummy __rte_unused)
+{
+	nfv_loop();
+	return 0;
+}
+
+/*
+ * Application main function - loops through
+ * receiving and processing packets. Never returns
+ */
+int
+main(int argc, char *argv[])
+{
+	const struct rte_memzone *mz;
+	int sock = SOCK_RESET;
+	unsigned int lcore_id;
+	unsigned int nb_ports;
+	int connected = 0;
+	char str[MSG_SIZE];
+	unsigned int i;
+	int flg_exit;  // used as res of parse_command() to exit if -1
+	int ret;
+
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		return -1;
+
+	argc -= ret;
+	argv += ret;
+
+	if (parse_app_args(argc, argv) < 0)
+		rte_exit(EXIT_FAILURE, "Invalid command-line arguments\n");
+
+	/* initialize port forward array*/
+	forward_array_init();
+	port_map_init();
+
+	/* Check that there is an even number of ports to send/receive on. */
+	nb_ports = rte_eth_dev_count_avail();
+	if (nb_ports > RTE_MAX_ETHPORTS)
+		nb_ports = RTE_MAX_ETHPORTS;
+
+	/* set up array for port data */
+	if (rte_eal_process_type() == RTE_PROC_SECONDARY) {
+		mz = rte_memzone_lookup(MZ_PORT_INFO);
+		if (mz == NULL)
+			rte_exit(EXIT_FAILURE,
+				"Cannot get port info structure\n");
+		ports = mz->addr;
+	} else { /* RTE_PROC_PRIMARY */
+		mz = rte_memzone_reserve(MZ_PORT_INFO, sizeof(*ports),
+			rte_socket_id(), NO_FLAGS);
+		if (mz == NULL)
+			rte_exit(EXIT_FAILURE,
+				"Cannot reserve memzone for port info\n");
+		memset(mz->addr, 0, sizeof(*ports));
+		ports = mz->addr;
+	}
+
+	set_user_log_debug(1);
+
+	RTE_LOG(INFO, APP, "Number of Ports: %d\n", nb_ports);
+
+	cmd = STOP;
+
+	/* update port_forward_array with active port */
+	for (i = 0; i < nb_ports; i++) {
+		if (!rte_eth_dev_is_valid_port(i))
+			continue;
+
+		/* Update ports_fwd_array with phy port*/
+		ports_fwd_array[i].in_port_id = i;
+		port_map[i].port_type = PHY;
+		port_map[i].id = i;
+		port_map[i].stats = &ports->port_stats[i];
+	}
+
+	lcore_id = 0;
+	RTE_LCORE_FOREACH_SLAVE(lcore_id) {
+		rte_eal_remote_launch(main_loop, NULL, lcore_id);
+	}
+
+	RTE_LOG(INFO, APP, "My ID %d start handling message\n", client_id);
+	RTE_LOG(INFO, APP, "[Press Ctrl-C to quit ...]\n");
+
+	/* send and receive msg loop */
+	while (on) {
+		ret = do_connection(&connected, &sock);
+		if (ret < 0) {
+			sleep(1);
+			continue;
+		}
+
+		ret = do_receive(&connected, &sock, str);
+		if (ret < 0)
+			continue;
+
+		RTE_LOG(DEBUG, APP, "Received string: %s\n", str);
+
+		flg_exit = parse_command(str);
+
+		/*Send the message back to client*/
+		ret = do_send(&connected, &sock, str);
+
+		if (flg_exit < 0)  /* terminate process if exit is called */
+			break;
+		else if (ret < 0)
+			continue;
+	}
+
+	/* exit */
+	close(sock);
+	sock = SOCK_RESET;
+	RTE_LOG(INFO, APP, "spp_nfv exit.\n");
+	return 0;
+}
