b46c74999e687f5a244b1a7df6badadd2addf42f
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2019-05-08 11:00:52 +0900

    spp_mirror: add specific libs in shared dir
    This patch is same as previous oen for spp_vf.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

---
+++ src/shared/secondary/spp_worker_th/mirror_deps.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2019 Nippon Telegraph and Telephone Corporation
+ */
+
+#ifndef __SPP_WORKER_TH_MIRROR_DEPS_H__
+#define __SPP_WORKER_TH_MIRROR_DEPS_H__
+
+#include "spp_proc.h"
+
+/**
+ * Update Mirror info
+ *
+ * @param component
+ *  The pointer to struct spp_component_info.@n
+ *  The data for updating the internal data of mirror.
+ *
+ * @retval SPP_RET_OK succeeded.
+ * @retval SPP_RET_NG failed.
+ */
+int spp_mirror_update(struct spp_component_info *component);
+
+/**
+ * Mirror get component status
+ *
+ * @param lcore_id
+ *  The logical core ID for forwarder and merger.
+ * @param id
+ *  The unique component ID.
+ * @param params
+ *  The pointer to struct spp_iterate_core_params.@n
+ *  Detailed data of mirror status.
+ *
+ * @retval SPP_RET_OK succeeded.
+ * @retval SPP_RET_NG failed.
+ */
+int spp_mirror_get_component_status(
+		unsigned int lcore_id, int id,
+		struct spp_iterate_core_params *params);
+
+#endif  /* __SPP_WORKER_TH_MIRROR_DEPS_H__ */
