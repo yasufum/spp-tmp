yasufum/spp-tmp

commit 310c51b918e96c80d6859ce1c34e12322860dd8f

spp_vf: initialize variable master_lcore
Compilation of spp_vf is failed because of uninitialization of
`master_lcore` variable if using optimize option. This patch is to fix
the issue not only spp_vf but also spp_mirror and spp_pcap.

Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/pcap/spp_pcap.c
@@ -927,6 +927,12 @@ main(int argc, char *argv[])
 	signal(SIGTERM, stop_process);
 	signal(SIGINT,  stop_process);
 
+	/**
+	 * It should be initialized outside of while loop, or failed to
+	 * compile because it is referred when finalize `g_core_info`.
+	 */
+	master_lcore = rte_get_master_lcore();
+
 	while (1) {
 		int ret_eal = rte_eal_init(argc, argv);
 		if (unlikely(ret_eal < 0))
@@ -1032,7 +1038,6 @@ main(int argc, char *argv[])
 		}
 
 		/* Set the status of main thread to idle */
-		master_lcore = rte_get_master_lcore();
 		g_core_info[master_lcore].status = SPPWK_LCORE_IDLING;
 		int ret_wait = check_core_status_wait(SPPWK_LCORE_IDLING);
 		if (unlikely(ret_wait != 0))

src/mirror/spp_mirror.c
@@ -505,6 +505,12 @@ main(int argc, char *argv[])
 	signal(SIGTERM, stop_process);
 	signal(SIGINT,  stop_process);
 
+	/**
+	 * It should be initialized outside of while loop, or failed to
+	 * compile because it is referred when finalize `g_core_info`.
+	 */
+	master_lcore = rte_get_master_lcore();
+
 	while (1) {
 		int ret_dpdk = rte_eal_init(argc, argv);
 		if (unlikely(ret_dpdk < 0))
@@ -517,8 +523,6 @@ main(int argc, char *argv[])
 		int ret_parse = parse_app_args(argc, argv);
 		if (unlikely(ret_parse != 0))
 			break;
-
-		master_lcore = rte_get_master_lcore();
 
 		if (sppwk_set_mng_data(&g_iface_info, g_component_info,
 					g_core_info, g_change_core,

src/vf/spp_vf.c
@@ -220,6 +220,12 @@ main(int argc, char *argv[])
 	signal(SIGTERM, stop_process);
 	signal(SIGINT,  stop_process);
 
+	/**
+	 * It should be initialized outside of while loop, or failed to
+	 * compile because it is referred when finalize `g_core_info`.
+	 */
+	master_lcore = rte_get_master_lcore();
+
 	while (1) {
 		int ret_dpdk = rte_eal_init(argc, argv);
 		if (unlikely(ret_dpdk < 0))
@@ -274,7 +280,6 @@ main(int argc, char *argv[])
 		}
 
 		/* Set the status of main thread to idle */
-		master_lcore = rte_get_master_lcore();
 		g_core_info[master_lcore].status = SPPWK_LCORE_IDLING;
 		int ret_wait = check_core_status_wait(SPPWK_LCORE_IDLING);
 		if (unlikely(ret_wait != SPP_RET_OK))

commit 0b4a7d354cfd5faf0a5177c0a18666dd600e8e80

spp_pcap: fix wrong master lcore other than 0
As previous patch, fix assigning wrong master lcore by moving
rte_eal_init() before rte_get_master_lcore().

Fixes: 310c51b918e9 ("spp_vf: initialize variable master_lcore")

Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/pcap/spp_pcap.c
@@ -905,7 +905,7 @@ slave_main(void *arg __attribute__ ((unused)))
 int
 main(int argc, char *argv[])
 {
-	int ret = SPPWK_RET_NG;
+	int ret;
 	char ctl_ip[IPADDR_LEN] = { 0 };
 	int ctl_port;
 	int ret_cmd_init;
@@ -927,20 +927,24 @@ main(int argc, char *argv[])
 	signal(SIGTERM, stop_process);
 	signal(SIGINT,  stop_process);
 
+	ret = rte_eal_init(argc, argv);
+	if (unlikely(ret < 0))
+		rte_exit(EXIT_FAILURE, "Invalid EAL arguments.\n");
+
+	argc -= ret;
+	argv += ret;
+
 	/**
 	 * It should be initialized outside of while loop, or failed to
 	 * compile because it is referred when finalize `g_core_info`.
 	 */
 	master_lcore = rte_get_master_lcore();
 
+	/**
+	 * If any failure is occured in the while block, break to go the end
+	 * of the block to finalize.
+	 */
 	while (1) {
-		int ret_eal = rte_eal_init(argc, argv);
-		if (unlikely(ret_eal < 0))
-			break;
-
-		argc -= ret_eal;
-		argv += ret_eal;
-
 		/* Parse spp_pcap specific parameters */
 		int ret_parse = parse_app_args(argc, argv);
 		if (unlikely(ret_parse != 0))