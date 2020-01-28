yasufum/spp-tmp

commit 146516c139c2736dee40d0fd6027bb3d060b7348

shared: add parsing ethdev name
This update is to add parser parse_dev_name() for the name of ethdev,
such as `eth_vhost1` or `net_ring1`. By using this function,
port type `VHOST` and ID `1` can be extracted from `eth_vhost1`.

This parser is intended to be used in initialization of ports in
secondary process in which all shared ports managed by spp_primary are
initialized as PHY incorrectly even if it is not PHY type. It is
because secondary expects the type of ports in spp_primary is always
PHY. However, port type might be other than PHY if spp_primary is
launched with `--vdev` option.

By using this parser, port type and ID can be inspected in secondary.

Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/shared/common.c
@@ -163,3 +163,69 @@ int set_spp_ctl_port(int s_port)
 	spp_ctl_port = s_port;
 	return 0;
 }
+
+/**
+ * Get port type and port ID from ethdev name, such as `eth_vhost1` which
+ * can be retrieved with rte_eth_dev_get_name_by_port().
+ * In this case of `eth_vhost1`, port type is `VHOST` and port ID is `1`.
+ */
+int parse_dev_name(char *dev_name, int *port_type, int *port_id)
+{
+	char pid_str[12] = { 0 };
+	int pid_len;
+	int dev_name_len = strlen(dev_name);
+	int dev_str_len;
+
+	if (strncmp(dev_name, VDEV_PREFIX_RING,
+			strlen(VDEV_PREFIX_RING)) == 0) {
+		dev_str_len = strlen(VDEV_PREFIX_RING);
+		pid_len = dev_name_len - dev_str_len;
+		strncpy(pid_str, dev_name + strlen(VDEV_PREFIX_RING),
+				pid_len);
+		*port_id = (int)strtol(pid_str, NULL, 10);
+		*port_type = RING;
+
+	} else if (strncmp(dev_name, VDEV_PREFIX_VHOST,
+			strlen(VDEV_PREFIX_VHOST)) == 0) {
+		dev_str_len = strlen(VDEV_PREFIX_VHOST);
+		pid_len = dev_name_len - dev_str_len;
+		strncpy(pid_str, dev_name + strlen(VDEV_PREFIX_VHOST),
+				pid_len);
+		*port_id = (int)strtol(pid_str, NULL, 10);
+		*port_type = VHOST;
+
+	} else if (strncmp(dev_name, VDEV_PREFIX_PCAP,
+			strlen(VDEV_PREFIX_PCAP)) == 0) {
+		dev_str_len = strlen(VDEV_PREFIX_PCAP);
+		pid_len = dev_name_len - dev_str_len;
+		strncpy(pid_str, dev_name + strlen(VDEV_PREFIX_PCAP),
+				pid_len);
+		*port_id = (int)strtol(pid_str, NULL, 10);
+		*port_type = PCAP;
+
+	} else if (strncmp(dev_name, VDEV_PREFIX_TAP,
+			strlen(VDEV_PREFIX_TAP)) == 0) {
+		dev_str_len = strlen(VDEV_PREFIX_TAP);
+		pid_len = dev_name_len - dev_str_len;
+		strncpy(pid_str, dev_name + strlen(VDEV_PREFIX_TAP),
+				pid_len);
+		*port_id = (int)strtol(pid_str, NULL, 10);
+		*port_type = TAP;
+
+	} else if (strncmp(dev_name, VDEV_PREFIX_NULL,
+			strlen(VDEV_PREFIX_NULL)) == 0) {
+		dev_str_len = strlen(VDEV_PREFIX_NULL);
+		pid_len = dev_name_len - dev_str_len;
+		strncpy(pid_str, dev_name + strlen(VDEV_PREFIX_NULL),
+				pid_len);
+		*port_id = (int)strtol(pid_str, NULL, 10);
+		*port_type = PCAP;
+
+	/* TODO(yasufum) add checking invalid port type and return -1 */
+	} else {
+		*port_id = 0;
+		*port_type = PHY;
+	}
+
+	return 0;
+}

src/shared/common.h
@@ -43,6 +43,13 @@
 #define RTE_MP_RX_DESC_DEFAULT 512
 #define RTE_MP_TX_DESC_DEFAULT 512
 
+#define VDEV_PREFIX_RING  "net_ring"
+#define VDEV_PREFIX_VHOST "eth_vhost"
+#define VDEV_PREFIX_PCAP  "net_pcap"
+#define VDEV_PREFIX_TAP   "net_tap"
+#define VDEV_PREFIX_NULL  "eth_null"
+
+
 /* Command. */
 enum cmd_type {
 	STOP,
@@ -82,6 +89,7 @@ enum port_type {
 	VHOST,
 	PCAP,
 	NULLPMD,
+	TAP,
 	UNDEF,
 };
 
@@ -171,5 +179,14 @@ int get_spp_ctl_port(void);
  * @return 0 if succeeded, or -1 if failed.
  */
 int set_spp_ctl_port(int s_port);
+
+/**
+ * Get port type and port ID from ethdev name, such as `eth_vhost1` which
+ * can be retrieved with rte_eth_dev_get_name_by_port().
+ * In this case of `eth_vhost1`, port type is `VHOST` and port ID is `1`.
+ *
+ * @return 0 if succeeded, or -1 if failed.
+ */
+int parse_dev_name(char *dev_name, int *port_type, int *port_id);
 
 #endif

commit 608c69c771d252ada05df2b605668154167dc76c

shared: fix wrong port_type in parsing dev name
Correct parse_dev_name() which returns port_type PCAP for eth_null
wrongly because of a typo. This patch is to fix to return NULLPMD
correctly.

Fixes: 146516c139c2 ("shared: add parsing ethdev name")

Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/shared/common.c
@@ -126,7 +126,7 @@ int parse_dev_name(char *dev_name, int *port_type, int *port_id)
 		strncpy(pid_str, dev_name + strlen(VDEV_ETH_NULL),
 				pid_len);
 		*port_id = (int)strtol(pid_str, NULL, 10);
-		*port_type = PCAP;
+		*port_type = NULLPMD;
 
 	/* TODO(yasufum) add checking invalid port type and return -1 */
 	} else {