yasufum/spp-tmp

commit 5094c9b88cf95804aa67ffd8fdf38a18167c7bb5

shared: switch to use vhost PMD for SPP
To switch vhost device to dedicated one, this patch replaces definitions
of the nanmes.

  * VHOST_BACKEND_NAME is changed from "eth_vhost%u" to "spp_vhost%u".
  * VDEV_ETH_VHOST is changed from "eth_vhost" to "spp_vhost".

Signed-off-by: Itsuro Oda <oda@valinux.co.jp>
Acked-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/shared/common.h
@@ -34,7 +34,7 @@
 
 #define VDEV_ETH_RING "eth_ring"
 #define VDEV_NET_RING "net_ring"
-#define VDEV_ETH_VHOST "eth_vhost"
+#define VDEV_ETH_VHOST "spp_vhost"
 #define VDEV_NET_VHOST "net_vhost"
 #define VDEV_PCAP "net_pcap"
 #define VDEV_ETH_TAP "eth_tap"

src/shared/secondary/add_port.h
@@ -10,7 +10,7 @@
 #define NR_DESCS 128
 
 #define VHOST_IFACE_NAME "/tmp/sock%u"
-#define VHOST_BACKEND_NAME "eth_vhost%u"
+#define VHOST_BACKEND_NAME "spp_vhost%u"
 
 #define PCAP_PMD_DEV_NAME "eth_pcap%u"
 #define NULL_PMD_DEV_NAME "eth_null%u"

commit 444b17ad6ddcbe2b30d81effb2833c0eea810c36

shared: enable use of eth_vhost
This patch enables spp_primary to use eth_vhost with --vdev option.

Fixes: 5094c9b88cf9 (shared: switch to use vhost PMD for SPP)

Signed-off-by: Itsuro Oda <oda@valinux.co.jp>
Acked-by: Yasufumi Ogawa <yasufum.o@gmail.com>

src/shared/common.c
@@ -91,7 +91,9 @@ int parse_dev_name(char *dev_name, int *port_type, int *port_id)
 	} else if (strncmp(dev_name, VDEV_ETH_VHOST,
 				strlen(VDEV_ETH_VHOST)) == 0 ||
 			strncmp(dev_name, VDEV_NET_VHOST,
-				strlen(VDEV_NET_VHOST)) == 0) {
+				strlen(VDEV_NET_VHOST)) == 0 ||
+			strncmp(dev_name, VDEV_SPP_VHOST,
+				strlen(VDEV_SPP_VHOST)) == 0) {
 		dev_str_len = strlen(VDEV_NET_VHOST);
 		pid_len = dev_name_len - dev_str_len;
 		strncpy(pid_str, dev_name + strlen(VDEV_NET_VHOST),

src/shared/common.h
@@ -34,8 +34,9 @@
 
 #define VDEV_ETH_RING "eth_ring"
 #define VDEV_NET_RING "net_ring"
-#define VDEV_ETH_VHOST "spp_vhost"
+#define VDEV_ETH_VHOST "eth_vhost"
 #define VDEV_NET_VHOST "net_vhost"
+#define VDEV_SPP_VHOST "spp_vhost"
 #define VDEV_NET_PCAP "net_pcap"
 #define VDEV_ETH_TAP "eth_tap"
 #define VDEV_NET_TAP "net_tap"